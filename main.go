360d3bfc29b5bd9596e3a84cbccb9a9857820cf2
MortezaFa <teezy10299@gmail.com>
2025-03-26 21:39:40 -0600

    refactor

--- main.go
+++ main.go
@@ -23,15 +23,10 @@ func main() {
 		}
 	}
 
-	cmd := exec.Command("osascript", "-e", `tell application "System Events" to get name of first application process whose frontmost is true`)
-	out, _ := cmd.Output()
-	s := string(out)
-	s = strings.TrimSpace(s)
-
-	if s == "anki" {
+	if isWindowActive("anki") {
 		isAnkiActive = true
 	} else {
-		println(string(out))
+		println("Anki not active lel")
 	}
 
 	if isAnkirunning && isAnkiActive {
@@ -42,10 +37,29 @@ func main() {
 
 			elapsed := time.Since(start)
 			fmt.Printf("Elapsed time: %s\n", elapsed)
+
+			if !isWindowActive("anki") {
+				println("exited Anki stopping timer!")
+				break
+			}
+
 		}
 	} else {
 		fmt.Printf("------------CANT START TIMER------------\n")
 		fmt.Printf("Anki is running: %v \nAnki is active: %v", isAnkirunning, isAnkiActive)
 	}
 
+}
+
+func isWindowActive(s string) bool {
+	cmd := exec.Command("osascript", "-e", `tell application "System Events" to get name of first application process whose frontmost is true`)
+	out, _ := cmd.Output()
+	str := string(out)
+	str = strings.TrimSpace(str)
+
+	if str == s {
+		return true
+	}
+
+	return false
 }
