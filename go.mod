a927ef40d5d285f4ef3e13d9f140da88e7a2ccb9
MortezaFa <teezy10299@gmail.com>
2025-03-26 20:46:40 -0600

    made it so it can detect if an app like Anki is running and active and start a timer accordingly

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module Kansa
 
-go 1.23
+go 1.23.0
+
+toolchain go1.23.4
+
+require github.com/mitchellh/go-ps v1.0.0
--- main.go
+++ main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	dn "github.com/mitchellh/go-ps"
+	"os/exec"
+	"strings"
+	"time"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	time.Sleep(5 * time.Second)
+
+	allPro, _ := dn.Processes()
+	isAnkirunning := false
+	isAnkiActive := false
+
+	for _, pro := range allPro {
+
+		if pro.Executable() == "anki" {
+			isAnkirunning = true
+		}
+	}
+
+	cmd := exec.Command("osascript", "-e", `tell application "System Events" to get name of first application process whose frontmost is true`)
+	out, _ := cmd.Output()
+	s := string(out)
+	s = strings.TrimSpace(s)
+
+	if s == "anki" {
+		isAnkiActive = true
+	} else {
+		println(string(out))
+	}
+
+	if isAnkirunning && isAnkiActive {
+		for {
+			start := time.Now()
+
+			time.Sleep(2 * time.Second)
+
+			elapsed := time.Since(start)
+			fmt.Printf("Elapsed time: %s\n", elapsed)
+		}
+	} else {
+		fmt.Printf("------------CANT START TIMER------------\n")
+		fmt.Printf("Anki is running: %v \nAnki is active: %v", isAnkirunning, isAnkiActive)
+	}
+
 }
